d35f0c52c010131053bee433c851c089d3962f6c
Juan C. Mateo Peña <juancarlosmateopena@outlook.com>
2023-09-01 08:43:00 -0400

    Segundos cambios

--- Pago de nomina/Program.cs
+++ Pago de nomina/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Pago_de_nomina
+namespace CalculadoraHorasTrabajadas
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Calculadora de Horas Trabajadas");
+
+            Console.Write("Ingrese la hora de inicio (formato HH:mm): ");
+            string horaInicioStr = Console.ReadLine();
+            DateTime horaInicio = DateTime.ParseExact(horaInicioStr, "HH:mm", null);
+
+            Console.Write("Ingrese la hora de finalización (formato HH:mm): ");
+            string horaFinStr = Console.ReadLine();
+            DateTime horaFin = DateTime.ParseExact(horaFinStr, "HH:mm", null);
+
+            // Cálculo de las horas trabajadas
+            TimeSpan horasTrabajadas = horaFin horaInicio;
+
+            Console.WriteLine($"Horas trabajadas: {horasTrabajadas.TotalHours:F2}");
+
+            Console.ReadLine();
         }
     }
 }
